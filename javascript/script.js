c26b9ba046f9f0b23ecabe0a4ea38cb17f65599e
Parvez-31 <parveztarafder31@gmail.com>
2025-02-08 07:21:54 +0530

    add html

--- javascript/script.js
+++ javascript/script.js
@@ -110,7 +110,20 @@ currencie.forEach(function (value, _, set) {
 */
 
 const displayMovements = function (movments) {
-  movments.forEach(function (mov, i) {});
+  containerMovements.innerHTML = '';
+
+  movments.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `<div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
 };
 
 displayMovements(account1.movements);
