5e4c4fe1b5af998120721a01453922621bf967b3
Parvez-31 <parveztarafder31@gmail.com>
2025-02-25 11:24:01 +0530

    Implement btnlone

--- javascript/script.js
+++ javascript/script.js
@@ -290,6 +290,25 @@ btnTransfer.addEventListener('click', e => {
   }
 });
 
+btnLoan.addEventListener('click', e => {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some(item => item >= amount * 0.1)
+  ) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', e => {
   e.preventDefault();
 
